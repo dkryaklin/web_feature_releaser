0e2d791452909c11798c8e307046fc44c1a35e8f
Dmitry Kryaklin <dmitry.kryaklin@behavox.com>
2019-05-13 20:55:15 +0300

    Updates

---
+++ precache-manifest.401e4e9319ea7673e1b4a78a52365b11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1a9c8d38a8dbe7663540b26f2469c2",
+    "url": "/web_feature_releaser/index.html"
+  },
+  {
+    "revision": "9f3daa2cfed7b9d25ffd",
+    "url": "/web_feature_releaser/static/css/main.f707316f.chunk.css"
+  },
+  {
+    "revision": "20a7ceb13774cdf3b24d",
+    "url": "/web_feature_releaser/static/js/2.1475d487.chunk.js"
+  },
+  {
+    "revision": "9f3daa2cfed7b9d25ffd",
+    "url": "/web_feature_releaser/static/js/main.a6bf3deb.chunk.js"
+  },
+  {
+    "revision": "51318d4ee4dffdd824ca",
+    "url": "/web_feature_releaser/static/js/runtime~main.c4e51812.js"
+  }
+]);
